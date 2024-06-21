397f6775d85f12a1af26688796aa1515bddb4a18
Kotalkhan366 <teamza366@gmail.com>
2024-06-21 16:51:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the number of base 16 in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+int main()
+{
+char hex_digit;
+for(hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+{
+putchair(hex_digit);
+}
+printf("\n");
+return 0;
+}
