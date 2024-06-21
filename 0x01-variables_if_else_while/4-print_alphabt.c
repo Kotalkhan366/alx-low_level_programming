c70c9e5e937ec82a0c4f8a0c3db485464a31df53
Kotalkhan366 <teamza366@gmail.com>
2024-06-21 16:19:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+int main()
+{
+char lowercase;
+for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+printf("\n");
+return 0;
+}
