2fb08afa22595f11427af889f8dd514bc41430f9
Kotalkhan366 <teamza366@gmail.com>
2024-07-09 10:37:54 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 /*
-* main - write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
+* main - A program that will print a[2] = 98
+* Return: Always 0 Success
 */
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
 
 int main(void)
 {
@@ -18,6 +12,9 @@ int *p;
 
 a[2] = 1024;
 p = &n;
+*p = 98;
 printf("a[2] = %d\n", a[2]);
-return (0);
+{
+return 0;
+}
 }
